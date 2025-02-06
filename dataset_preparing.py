8346ee1c3a06b2fea17854fddd230ec0f6005487
veil <auphong2707@gmail.com>
2025-02-06 11:47:37 +0700

    Accept change from Kaggle Test

--- dataset_preparing.py
+++ dataset_preparing.py
@@ -1,11 +1,19 @@
-from transformers import T5Tokenizer
-from datasets import Dataset
+import os
+import json
 
-tokenizer = T5Tokenizer.from_pretrained("Salesforce/codet5-base")
+from transformers import RobertaTokenizer
+from datasets import Dataset
+from huggingface_hub import snapshot_download, login
+
+tokenizer = RobertaTokenizer.from_pretrained("Salesforce/codet5-base")
 
 # Example input-output pair
 # input_text = "Summarize: ~Code~"
 # output_text = "Sumarized Content"
+
+def load_jsonl(file_path):
+    with open(file_path, 'r') as file:
+        return [json.loads(line) for line in file]
 
 def preprocess_data(data_point):
     # Convert code tokens into a string (space-separated)
@@ -23,8 +31,8 @@ def tokenize_data(data_point):
     input_text, target_summary = preprocess_data(data_point)
 
     # Tokenize input code and output summary
-    model_inputs = tokenizer(input_text, max_length=512, truncation=True, padding='max_length')
-    labels = tokenizer(target_summary, max_length=256, truncation=True, padding='max_length')
+    model_inputs = tokenizer(input_text, max_length=200, truncation=True, padding='max_length')
+    labels = tokenizer(target_summary, max_length=100, truncation=True, padding='max_length')
 
     # Add labels
     model_inputs['labels'] = labels['input_ids']
@@ -32,13 +40,30 @@ def tokenize_data(data_point):
     return model_inputs
 
 
-def prepare_dataset(train_data, val_data):
+def prepare_dataset():
+    # Download the dataset
+    login(token=os.getenv("HUGGINGFACE_TOKEN"))
+    snapshot_download(repo_id="auphong2707/dl4se-code-review-dataset", 
+                      repo_type="dataset",
+                      local_dir=".",
+                      allow_patterns="*.jsonl"
+                     )
+
+    # Load data
+    train_data = load_jsonl('data/Review-Comment-Generation/trans_data/train.jsonl')
+    val_data = load_jsonl('data/Review-Comment-Generation/trans_data/val.jsonl')
+
     # Convert data into a Dataset object
     train_dataset = Dataset.from_list(train_data)
     val_dataset = Dataset.from_list(val_data)
 
     # Tokenize the data
-    train_dataset = train_dataset.map(tokenize_data, batched=True)
-    val_dataset = val_dataset.map(tokenize_data, batched=True)
+    train_dataset = train_dataset.map(tokenize_data, batched=False, remove_columns=train_dataset.column_names)
+    val_dataset = val_dataset.map(tokenize_data, batched=False, remove_columns=val_dataset.column_names)
 
     return train_dataset, val_dataset, tokenizer
+
+if __name__ == "__main__":
+    train_dataset, val_dataset, tokenizer = prepare_dataset()
+    print(train_dataset[0])
+    print(val_dataset[0])
--- main.py
+++ main.py
@@ -1,35 +1,53 @@
 from transformers import TrainingArguments, Trainer, T5ForConditionalGeneration
 from dataset_preparing import prepare_dataset
-import json
+import wandb, os
+import evaluate
 
 # Prepare dataset
-def load_jsonl(file_path):
-    with open(file_path, 'r') as file:
-        return [json.loads(line) for line in file]
+train_dataset, val_dataset, tokenizer = prepare_dataset()
 
-train_data = load_jsonl('data/Review-Comment-Generation/trans_data/train.jsonl')
-val_data = load_jsonl('data/Review-Comment-Generation/trans_data/train.jsonl')
+# Define compute_metrics function
+metric = evaluate.load("bleu")
 
-train_dataset, val_dataset, tokenizer = prepare_dataset(train_data, val_data)
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+
+    # Decode the predictions and labels
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # Calculate BLEU score
+    bleu_score = metric.compute(predictions=decoded_preds, references=[[label] for label in decoded_labels])
+
+    return bleu_score
+
+# Login wandb
+wandb.login(key=os.getenv("WANDB_API_KEY"))
 
 # Create training arguments
 training_args = TrainingArguments(
-    output_dir='./results/code-summarization/code-t5-base',
-    evaluation_strategy='epochs',
+    output_dir='./results/code-summarization/code-t5-small',
+    run_name='test-codet5-small',
+    report_to="wandb",
+    eval_strategy='epoch',
     save_strategy='epoch',
     learning_rate=3e-4,
     per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=100,
+    per_device_eval_batch_size=1,
+    num_train_epochs=1,
     weight_decay=0.01,
-    logging_dir='./logs/code-summarization/code-t5-base',
-    logging_steps=10,
+    eval_accumulation_steps=256,
+    torch_empty_cache_steps=128,
+    logging_dir='./logs/code-summarization/code-t5-small',
+    logging_steps=100,
     save_total_limit=2,
-    fp16=True, # Enable mixed precision training
+    fp16=True
 )
 
 # Load model
-model = T5ForConditionalGeneration.from_pretrained("Salesforce/codet5-base")
+model = T5ForConditionalGeneration.from_pretrained('Salesforce/codet5-small')
+model.generation_config.max_length = 100
+model.generation_config.num_beams = 3
 
 # Create Trainer
 trainer = Trainer(
@@ -37,11 +55,13 @@ trainer = Trainer(
     args=training_args,
     train_dataset=train_dataset,
     eval_dataset=val_dataset,
+    preprocess_logits_for_metrics=(lambda logits, labels: logits[0].argmax(dim=-1)),
+    compute_metrics=compute_metrics,
 )
 
 # Train the model
 trainer.train()
 
 # Save Fined-Tuned Model
-model.save_pretrained('./results/code-summarization/code-t5-base')
-tokenizer.save_pretrained('./results/code-summarization/code-t5-base')
+model.save_pretrained('./results/code-summarization/code-t5-small')
+tokenizer.save_pretrained('./results/code-summarization/code-t5-small')
